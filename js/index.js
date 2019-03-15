dc06e984aabc4c750554e5d9318f5282ea7607e9
Lilia Piontyk <pyontiklilia98@gmail.com>
2019-03-15 15:32:07 +0000

    Created initial markup

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
-document.querySelector('.feedback').addEventListener('submit', submitForm);
+document.querySelector('.inner_contact').addEventListener('submit', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
     fetch('form.php', {
         method: 'POST',
-        body: new FormData(document.querySelector('.feedback'))
+        body: new FormData(document.querySelector('.inner_contact'))
     })
         .then( response => response.text() )
         .then( html => document.querySelector('.server-response')
 .innerHTML = html );
 }
-
-
-const clockPlaceholder = document.querySelector('.clock');
-setInterval( () => clockPlaceholder.innerText = (new Date()).toLocaleTimeString() ,
-  1000);
-  
   
